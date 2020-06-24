901748240d6e4d0e7b261851fd82c45f8e07e26b
sbsteacher <pirbak@daum.net>
2020-06-24 14:05:29 +0900

    글쓰기 화면 띄우기

--- board/src/com/koreait/board/BoardWriteSer.java
+++ board/src/com/koreait/board/BoardWriteSer.java
@@ -1,27 +1,22 @@
 package com.koreait.board;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.koreait.board.db.BoardDAO;
-import com.koreait.board.model.BoardVO;
-
-@WebServlet("/boardList")
-public class BoardListSer extends HttpServlet {
+@WebServlet("/boardWrite")
+public class BoardWriteSer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    	
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/boardList.jsp");
-		rd.forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/jsp/boardWrite.jsp").forward(request, response);
 	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
 }
-
-
-
