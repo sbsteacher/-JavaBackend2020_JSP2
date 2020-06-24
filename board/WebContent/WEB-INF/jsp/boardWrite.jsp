2369f14388142d575e19f0042567a8195adb151e
sbsteacher <pirbak@daum.net>
2020-06-24 15:03:53 +0900

    글쓰기 완료

--- board/WebContent/WEB-INF/jsp/boardWrite.jsp
+++ board/WebContent/WEB-INF/jsp/boardWrite.jsp
@@ -8,5 +8,24 @@
 </head>
 <body>
 	<div>글쓰기 화면</div>
+	<div>
+		<form id="frm" action="/boardWrite" method="post" onsubmit="chk()">
+			<div>
+				<label>제목 : <input type="text" name="title"></label>
+			</div>
+			<div>
+				<textarea name="ctnt"></textarea>
+			</div>
+			<div>				
+				<input type="submit" value="저장">
+				<input type="reset" value="새로작성">
+			</div>
+		</form>	
+	</div>
+	<script>
+		function chk() {
+			alert('체크!!')	
+		}
+	</script>
 </body>
 </html>
--- board/src/com/koreait/board/BoardWriteSer.java
+++ board/src/com/koreait/board/BoardWriteSer.java
@@ -1,11 +1,15 @@
 package com.koreait.board;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.koreait.board.db.BoardDAO;
+import com.koreait.board.model.BoardVO;
 
 @WebServlet("/boardWrite")
 public class BoardWriteSer extends HttpServlet {
@@ -16,7 +20,26 @@ public class BoardWriteSer extends HttpServlet {
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String title = request.getParameter("title");
+		String ctnt = request.getParameter("ctnt");
+		
+		System.out.println("title : " + title);
+		System.out.println("ctnt : " + ctnt);
+		
+		BoardVO vo = new BoardVO();
+		vo.setTitle(title);
+		vo.setCtnt(ctnt);
+		
+		BoardDAO.insertBoard(vo);
+		
+		response.sendRedirect("/boardList");
 		
 	}
 
 }
+
+
+
+
+
+
